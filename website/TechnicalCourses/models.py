c059546e4cc3b4f4da7f1cc054398b8e85203453
Thakuriprince <princethakuri11111@gmail.com>
2020-09-01 17:31:00 +0545

    update

--- website/TechnicalCourses/models.py
+++ website/TechnicalCourses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Allcourses(models.Model):
+    coursename = models.CharField(max_length=200)
+    insname = models.CharField(max_length=100)
+    def __str__(self):
+        return self.coursename
+
+
+
+class details(models.Model):
+    course = models.ForeignKey(Allcourses, on_delete= models.CASCADE)
+    sp = models.CharField(max_length=200)
+    il = models.CharField(max_length=200)
+    def _str_(self):
+        return self.sp
+
+        
+
